98519628e27b7d7e4c85c2b0f9021c50ee3147af
Caio Cesar Pereira Dias Batista <caiocesar9920@gmail.com>
2025-05-29 17:53:17 -0300

    Custom SQL Query

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,7 +1,7 @@
 package com.devsuperior.dslist.dto;
 
-
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -11,7 +11,7 @@ public class GameMinDTO {
         private String imgUrl;
         private String shortDescription;
 
-        public GameMinDTO(){
+        public GameMinDTO() {
         }
 
         public GameMinDTO(Game entity) {
@@ -20,6 +20,14 @@ public class GameMinDTO {
                 year = entity.getYear();
                 imgUrl = entity.getImgUrl();
                 shortDescription = entity.getShortDescription();
+        }
+
+        public GameMinDTO(GameMinProjection projection) {
+                id = projection.getId();
+                title = projection.getTitle();
+                year = projection.getYear();
+                imgUrl = projection.getImgUrl();
+                shortDescription = projection.getShortDescription();
         }
 
         public Long getId() {
